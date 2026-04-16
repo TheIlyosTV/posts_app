19ba4c0de7ed09545142603c77c41dab9b10cb49
Axmedjanov Ilyosbek <theilyostv@gmail.com>
2026-04-16 21:41:55 +0500

    Fix migrations for PostgreSQL compatibility

--- db/cable_migrate/20260416150001_create_solid_cable_tables.rb
+++ db/cable_migrate/20260416150001_create_solid_cable_tables.rb
@@ -2,7 +2,7 @@
 
 class CreateSolidCableTables < ActiveRecord::Migration[7.1]
   def change
-    create_table :solid_cable_messages do |t|
+    create_table :solid_cable_messages, id: :bigint do |t|
       t.binary :channel, limit: 1024, null: false
       t.binary :payload, limit: 536870912, null: false
       t.datetime :created_at, null: false
